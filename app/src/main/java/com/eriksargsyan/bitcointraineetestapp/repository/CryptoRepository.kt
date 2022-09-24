9b04693dd0d2795b78ef767aae6ca97b21d82ebe
prdumbledore <vjqljv29>
2022-09-25 00:28:16 +0300

    Fix NetworkAccess & StringTransformer
    Fixing fun hasNetworkAccess (replace deprecated method)
    Fixing fun currentPriceToStr with String.format

--- app/src/main/java/com/eriksargsyan/bitcointraineetestapp/repository/CryptoRepository.kt
+++ app/src/main/java/com/eriksargsyan/bitcointraineetestapp/repository/CryptoRepository.kt
@@ -2,9 +2,9 @@ package com.eriksargsyan.bitcointraineetestapp.repository
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import com.eriksargsyan.bitcointraineetestapp.model.domain.Crypto
 import com.eriksargsyan.bitcointraineetestapp.model.domain.CryptoDetails
-import com.eriksargsyan.bitcointraineetestapp.model.network.CryptoDetailsNet
 import com.eriksargsyan.bitcointraineetestapp.network.CryptoAPI
 import com.eriksargsyan.bitcointraineetestapp.util.IO
 import com.eriksargsyan.bitcointraineetestapp.util.NetworkDetailsMapper
@@ -43,13 +43,19 @@ class CryptoRepositoryImpl @Inject constructor(
         }
     }
 
-
     override suspend fun hasNetworkAccess(): Boolean {
         delay(1000)
-        val connectivityManager: ConnectivityManager? =
-            appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
-        val activeNetworkInfo = connectivityManager!!.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        val connectivityManager =
+            appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
     }
 
 }
--- app/src/main/java/com/eriksargsyan/bitcointraineetestapp/util/StringTransformer.kt
+++ app/src/main/java/com/eriksargsyan/bitcointraineetestapp/util/StringTransformer.kt
@@ -8,17 +8,8 @@ import kotlin.math.abs
 object StringTransformer {
 
     fun currentPriceToStr(currentPrice: Double): String {
-        val strList = currentPrice.toString().split(".")
-        val sb = StringBuilder()
-        var str = strList[0].reversed()
-        while (str.length > 3) {
-            sb.append("${str.take(3)},")
-            str = str.drop(3)
-        }
-        sb.append(str)
-        sb.reverse()
-
-        return sb.append(".${strList[1].take(3)}").toString()
+        return if (currentPrice.toInt() >= 1) String.format("%,.2f", currentPrice)
+        else String.format("%,.5f", currentPrice)
     }
 
     fun priceChangeToStr(priceChange: Double): String {
